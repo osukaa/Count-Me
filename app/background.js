24dea6c0550d411e3c64dedf338c31502e4aa846
Oscar Funes <osukaa18@gmail.com>
2013-05-20 21:35:19 -0600

    Add popup style sheet, background.js no longer has a +1 on the tabcount

--- app/background.js
+++ app/background.js
@@ -1,20 +1,25 @@
 tabCount = 0;
 
 function updateBadgeText(){
+  console.log('update');
   chrome.browserAction.setBadgeText({text: String(tabCount)});
 }
 
 chrome.windows.getCurrent({populate: true},function (window){
+  console.log('init');
   tabCount = window.tabs.length;
+  console.log(tabCount);
   updateBadgeText();
-});
+})
 
 chrome.tabs.onCreated.addListener(function(tab){
   tabCount += 1;
+  console.log(tabCount);
   updateBadgeText();
 });
 
 chrome.tabs.onRemoved.addListener(function(tab){
   tabCount -= 1;
+  console.log(tabCount);
   updateBadgeText();
 });
